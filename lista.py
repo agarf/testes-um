799bea2f4d0aba20b8b0988a1a51c53cc3f5f6c8
ronaldo fraga da silva <fotografofraga@gmail.com>
2024-07-18 16:25:21 -0300

    Update lista.py
    Fim das varias funcoes lista.

--- lista.py
+++ lista.py
@@ -1,57 +1,75 @@
 import time
 
 print('LISTAS')
-time.sleep(5)
 num = [2, 5, 9, 1]
 print(num)
 print('')
+time.sleep(10)
 
 print('ADICIONAR UM NUMERO A LISTA')
-time.sleep(5)
 num.append(7)
 print(num)
 print('')
+time.sleep(10)
 
 print('COLOCAR A LISTA EM ORDEM')
-time.sleep(5)
 num.sort()
 print(num)
 print('')
+time.sleep(10)
 
 print('LISTA DE TRAZ PRA FRENTE')
-time.sleep(5)
 num.sort(reverse=True)
 print(num)
 print('')
+time.sleep(10)
 
 print('LISTA É MUTAVEL')
-time.sleep(5)
 num[2] = 3
 print(num)
 print('')
+time.sleep(10)
 
 print('CONTAR OS ELEMENTOS COM LEN')
-time.sleep(5)
 print(f'Essa lista {len(num)} elementos')
 print('')
+time.sleep(10)
 
 print('INSERIR VALORES EM UMA POSIÇAO')
-time.sleep(5)
 num.insert(2, 0)
 print(num)
 print('')
+time.sleep(10)
 
 print('REMOVER O ÚLTIMO VALOR COM O COMANDO POP')
-time.sleep(5)
 num.pop()
 print(num)
 print('')
+time.sleep(10)
 
 print('REMOVE O ELEMENTO 2 DA LISTA')
-time.sleep(10)
 num.pop(2)
 print(num)
 print('')
+time.sleep(10)
 
-print('parei no minuto 19.20')
+print('INSERIR MAIS UM 2')
+num.insert(2, 2)
+print(num)
+print('')
+time.sleep(10)
 
+print('COMANDO REMOVE O NUMERO 2')
+num.remove(2)
+print(num)
+print('')
+time.sleep(10)
+
+print('REMOVER UM NUMERO QUE PODE NAO ESTAR NA LISTA')
+if 4 in num:
+    num.remove(4)
+else:
+    print('Nao achei o número 4')
+time.sleep(10)
+
+print('FIM')
